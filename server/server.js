5bb32af3df6fef3672b289c14b5f4c35bcaaaf0a
thienhuyz <thienhuyz3006@gmail.com>
2025-10-18 23:39:29 +0700

    #2 Database Connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
 
 
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+dbConnect()
 
 app.listen(port, () => {
     console.log('Server running on the port: ' + port)
